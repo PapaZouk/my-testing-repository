3dd8eb958e4f6dd6210bba0bbcbdd94ee0fdf287
PapaZouk <rafexpapala@gmail.com>
2023-03-25 20:45:41 +0100

    Adds name for Dog

--- src/main/java/pl/zajavka/case1/Dog.java
+++ src/main/java/pl/zajavka/case1/Dog.java
@@ -6,4 +6,9 @@ public class Dog extends Animal {
     public void sniff() {
         System.out.println("I'm soo sniffing!");
     }
+
+    @Override
+    public String whatIsYourName() {
+        return "My name is " + name;
+    }
 }
